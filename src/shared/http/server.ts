5db0cb687fbb10ef8ddc4ff4fe23ad0b791fd254
andersonnaz <anderson_1004@hotmail.com>
2023-03-02 18:44:37 -0300

    build: :hammer: added class to handle errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,33 @@
-console.log('Hello dev');
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import routes from './routes/index';
+import { AppError } from '@shared/errors/appError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error!',
+        });
+    },
+);
+
+app.listen(3000, () => {
+    console.log('app running at port 3000');
+});
