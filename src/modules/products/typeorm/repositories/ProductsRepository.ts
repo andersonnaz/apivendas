140ad41f1ecbafa08eb966c2cfaaef3e336c6b65
andersonnaz <anderson_1004@hotmail.com>
2023-03-20 19:51:46 -0300

    fix: :bug: ProductRepository bug fixed

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -3,8 +3,8 @@ import Product from '../entities/Product';
 
 export const ProductRepository = dataSource.getRepository(Product).extend({
     async findByName(name: string) {
-        return this.createQueryBuilder('product')
-            .where('product.name = :name', { name })
+        return this.createQueryBuilder('products')
+            .where('products.name = :name', { name })
             .getMany();
     },
 });
