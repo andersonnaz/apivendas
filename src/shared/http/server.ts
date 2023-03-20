42dc16b0005ecf81eb140d10d7cd18fe8ad02dd0
andersonnaz <anderson_1004@hotmail.com>
2023-03-20 19:55:49 -0300

    build: :hammer: added middleware to data validation

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes/index';
 import { AppError } from '@shared/errors/appError';
 import { dataSource } from '../typeorm';
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 app.use(
     (
         error: Error,
