c51dc7cc3155f1542acd41cd96b837c1b8a56ac8
andersonnaz <anderson_1004@hotmail.com>
2023-03-23 14:59:08 -0300

    build: :hammer: migration, entity and datasource config of Users added.

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,6 +1,8 @@
+import User from '@modules/users/typeorm/entities/User';
 import { DataSource } from 'typeorm';
 import Product from '../../modules/products/typeorm/entities/Product';
 import { CreateProducts1678396605521 } from './migrations/1678396605521-CreateProducts';
+import { CreateUsers1679512263206 } from './migrations/1679512263206-CreateUsers';
 
 export const dataSource = new DataSource({
     type: 'postgres',
@@ -9,6 +11,6 @@ export const dataSource = new DataSource({
     username: 'postgres',
     password: 'docker',
     database: 'postgres',
-    entities: [Product],
-    migrations: [CreateProducts1678396605521],
+    entities: [Product, User],
+    migrations: [CreateProducts1678396605521, CreateUsers1679512263206],
 });
